0f558eca399c3edf4b94f316093d6619a933d81b
Tri Hieu <phamgiatrihieu@gmail.com>
2023-07-28 08:32:44 +0700

    change API endpoint

--- fe/src/store/url.js
+++ fe/src/store/url.js
@@ -1,1 +1,3 @@
-export const BASE_URL = 'http://18.140.101.238:8081';
+// https://danielaws.tk/group1
+// export const BASE_URL = 'http://18.140.101.238:8081';
+export const BASE_URL = 'https://danielaws.tk/group1';
